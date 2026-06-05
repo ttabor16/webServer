1c8e7d85c43b800813df933299f1aae64230c71a
ttabor16 <ttabor16@gmail.com>
2026-06-04 16:10:39 -1000

    Complete Ch1 L12 Custom handlers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import {Request, Response} from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static('.'));
+app.use("/app", express.static('./src/app'));
+
+app.get("/healthz", (req, res) => {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send('OK')
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
