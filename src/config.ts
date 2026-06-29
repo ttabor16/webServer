66848a772ae57401fc4a162f68a739e488a6993b
ttabor16 <ttabor16@gmail.com>
2026-06-28 17:18:19 -1000

    Complete Ch6 L3 Drizzle Queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile()
+
 export type APIConfig = {
   fileserverHits: number;
+  dbURL:string;
 };
 
-export const config = {
-    fileserverHits: 0
+export function envOrThrow(key: string): string{
+  const db = process.env[key];
+  if (!db) {
+    throw new Error("no db");
+  }
+  return db;
+}
+
+export const config: APIConfig = {
+    fileserverHits: 0,
+    dbURL:envOrThrow("DB_URL")
 };
